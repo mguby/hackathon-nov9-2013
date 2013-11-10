bc1637f77e56682ee9f397cc0aed24a5bcd35b3d
cjameszhang <cjameszhang@gmail.com>
2013-11-09 23:14:17 -0600

    11:14

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,108 +1,57 @@
 package gui;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.awt.Frame;
+import java.awt.Graphics;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-import com.restfb.Connection;
-import com.restfb.DefaultFacebookClient;
-import com.restfb.Facebook;
-import com.restfb.FacebookClient;
-import com.restfb.exception.FacebookException;
-import com.restfb.types.Page;
-import com.restfb.types.Post;
-import com.restfb.types.User;
+import facebook.Input;
 
-public class MainWindow extends JFrame {
-	/**
-	   * RestFB Graph API client.
-	   */
-	private final FacebookClient facebookClient;
+public class MainWindow extends JPanel implements Runnable {
+	private static final long serialVersionUID = 1L;
+	
+	private static final int SLEEP_TIME = 20;
 
-	/**
-	 * Entry point. You must provide a single argument on the command line: a
-	 * valid Graph API access token.
-	 * 
-	 * @param args
-	 *          Command-line arguments.
-	 * @throws FacebookException
-	 *           If an error occurs while talking to the Facebook Graph API.
-	 */
-	public static void main(String[] args) throws FacebookException {
-		Scanner in = new Scanner(System.in);
-		new MainWindow(in.nextLine()).runEverything();
+	public static void main(String[] args) {
+		JFrame window = new JFrame();
+		MainWindow content = new MainWindow();
+
+		window.setExtendedState(Frame.MAXIMIZED_BOTH);
+		window.setUndecorated(true);
+		
+		window.setContentPane(content);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setLayout(null);
+		window.setTitle("Family Tree");
+		
+		window.setVisible(true);
 	}
-
-	MainWindow(String accessToken) {
-		facebookClient = new DefaultFacebookClient(accessToken);
+	
+	public MainWindow() {
+		new Thread(this).start();
+		
+		Input.login("CAACEdEose0cBAFozaiyIU3sZCicBej8QqoyPYI0FxokPxkkZB0G0eibNgVHZCqR0ADDT37L6mTIv5g9HcfXoKZAIq6DZAMtXGAOGvmsMaGW4vSJ2khJKxWQFqmv8fesSW6tRu9obYrqr2biyS8h1ZCc6RKzNl4ffeNTZAZCCIf63p98kb9TLa3LlWsqBBjarTRYqOE8pjl8mvgZDZD");
+		Input.initialize();
 	}
-
-	void runEverything() throws FacebookException {
-		fetchConnections();
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		int W = this.getWidth(), H = this.getHeight();
+		g.drawImage(Input.head.picture, W/2-80, H/2-80, 150, 150, null);
 	}
-
-	void fetchConnections() throws FacebookException {
-		for (int i = 0; i < 4; i++)
-			names.add(new ArrayList<String>());
-		List<FqlUser> users = facebookClient.executeFqlQuery(
-				"SELECT uid, name, relationship FROM family WHERE profile_id = me()", FqlUser.class);
-
-		recurse("base", users, 3);
-
-		for (int i = 0; i < names.size(); i++) {
-			System.out.println("GENERATION " + i);
-			for (int j = 0; j < names.get(i).size(); j++) {
-				System.out.println(names.get(i).get(j));
-			}
-			System.out.println();
-		}
-	}
-
-	ArrayList<ArrayList<String>> names = new ArrayList<ArrayList<String>>();
-
-	void recurse(String pre, List<FqlUser> u, int gen) {
-		if (gen == 0)
-			return;
-
-		for (FqlUser us : u) {
-			if (gen == 3)
-				System.out.println(us.name + "-" + us.uid +"\n"+"\n"+"\n"+"\n");
-			names.get(3 - gen).add(us.name);
+	
+	@Override
+	public void run() {
+		long start;
+		
+		while (true) {
+			start = System.currentTimeMillis();
+			
 			try {
-				List<FqlUser> users = facebookClient.executeFqlQuery(
-						"SELECT uid, name, relationship FROM family WHERE profile_id = " + us.uid, FqlUser.class);
-				if (us.name.contains("ichelle") && gen == 3) {
-					System.out.println("!!!!!!!");
-					System.out.println(users.size() + " " + users.get(0).toString());
-					System.out.println("!!!!!!!");
-				}
-				recurse(pre + " " + us.name, users, gen - 1);
+				Thread.sleep(Math.max(0, start + SLEEP_TIME - System.currentTimeMillis()));
 			} catch (Exception e) {
-				System.out.println(e.getMessage());
 			}
 		}
 	}
-
-	public static class FqlUser {
-		@Facebook
-		String uid;
-
-		@Facebook
-		String name;
-
-		@Facebook
-		String relationship;
-
-		@Facebook
-		String pic_square;
-
-		@Override
-		public String toString() {
-			return String.format("%s %s (%s) %s", name, relationship, uid, pic_square);
-		}
-	}
-
 }
